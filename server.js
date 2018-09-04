ba1fbbc987143e0a914047ae4cd9fbd5f916b70d
Daria Gonsalves <daria.gonsalves@hyuna.bb>
2018-09-04 09:37:20 -0400

    Adding node web server

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000; //if port is not set use 3000 instead
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('Maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express</h1>');
+  // res.send({
+  //   name: 'Daria',
+  //   likes: [
+  //     'fish',
+  //     'food'
+  //   ]
+  // })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+      welcomeMessage: 'Welcome to the home page',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+      pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Error while handling this request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
